78920c5f230ebc29c21e2112016f310ff278635a
Mattias Appelgren <mattias@ppelgren.se>
2020-05-21 14:19:37 +0200

    Fix kubeconfig copy

--- static/main.js
+++ static/main.js
@@ -9,21 +9,17 @@ var app = new Vue({
             window.location.href = "/login/spotify";
         },
         logout: function() {
-          fetch("/logout")
-              .then(stream => stream.json())
-              .then(function(data) {
-                  if (data.ok === true){
-                    app.authed = false;
-                  }
-              });
+            fetch("/logout")
+                .then(stream => stream.json())
+                .then(function(data) {
+                    if (data.ok === true) {
+                        app.authed = false;
+                    }
+                });
         },
-        selectAndCopyKubeconfig: function() {
-            var kc = document.getElementById("kubeconfig");
-            //kc.select();
-            navigator.clipboard.writeText(kc.textContent).then(function() {
-                console.log('Copy successful!');
-            }, function(err) {
-                console.error('Could not copy text: ', err);
+        copyKubeconfig: function() {
+            navigator.clipboard.writeText(app.user.kubeconfig).catch(error => {
+                console.error('Could not copy to clipboard: ', error);
             });
         }
     },
@@ -31,9 +27,9 @@ var app = new Vue({
         fetch("/user")
             .then(stream => stream.json())
             .then(function(data) {
-            if (data.error === false && "name" in data){
-                app.authed = true;
-                app.user = data;
+                if (data.error === false && "name" in data) {
+                    app.authed = true;
+                    app.user = data;
                 }
             });
     }
